c1fa0c332c276f777ce88921ab72cea20e2ce2b3
Roman Lomovtsev <coutvv@gmail.com>
2024-01-18 12:35:16 +0300

    add syntax highlighting for xit files

---
+++ src/main/kotlin/com/lomovtsev/xitsupport/XitSyntaxHighlighter.kt
@@ -0,0 +1,41 @@
+package com.lomovtsev.xitsupport
+
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+import com.lomovtsev.xitsupport.psi.XitTypes
+
+class XitSyntaxHighlighter : SyntaxHighlighterBase() {
+
+    val SEPARATOR = createTextAttributesKey("XIT_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
+
+    val TASK = createTextAttributesKey("XIT_TASK", DefaultLanguageHighlighterColors.KEYWORD)
+
+    val TEXT = createTextAttributesKey("XIT_TEXT", DefaultLanguageHighlighterColors.STRING)
+
+    val BAD_CHAR = createTextAttributesKey("XIT_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+
+    val SEPARATOR_KEYS = arrayOf(SEPARATOR)
+    val TASK_KEYS = arrayOf(TASK)
+    val TEXT_KEYS = arrayOf(TEXT)
+    val BAD_KEYS = arrayOf(BAD_CHAR)
+
+    val EMPTY_KEYS = emptyArray<TextAttributesKey>()
+    override fun getHighlightingLexer(): Lexer {
+        return XitLexerAdapter()
+    }
+
+    override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
+        return when (tokenType) {
+            XitTypes.POINT -> TASK_KEYS
+            XitTypes.TEXT -> TEXT_KEYS
+            TokenType.BAD_CHARACTER -> BAD_KEYS
+            else -> EMPTY_KEYS
+        }
+    }
+}
